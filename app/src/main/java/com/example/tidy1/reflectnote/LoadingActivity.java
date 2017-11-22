79d8e56f069984486dba4da13e676a2ff61f525b
gotaehyuk <gotaehyuk@gmail.com>
2017-11-22 13:35:22 +0900

    c

--- app/src/main/java/com/example/tidy1/reflectnote/LoadingActivity.java
+++ app/src/main/java/com/example/tidy1/reflectnote/LoadingActivity.java
@@ -1,5 +1,7 @@
 package com.example.tidy1.reflectnote;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        Handler handler= new Handler();
+        handler.postDelayed(new Runnable(){
+                                public void run(){
+                                    Intent intent = new Intent(getBaseContext(),LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();}}
+
+                ,3000);{}
     }
 }
